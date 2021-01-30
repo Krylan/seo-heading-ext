868772cdbb3e8fcce9dd87d9361a3063ade9b026
Krylan <drapeg1@gmail.com>
2021-01-30 21:15:33 +0100

    add error box, start working immediately after opening extension, optimize code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getHeadings() {
 	let headings = ['','','','','',''];
 	let headingsCount = [0,0,0,0,0,0];
 	let i = 1;
-	let ghostIcon = '<span class="label hidden" title="This heading is non-visible (directly or by parents\' styles)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.885 14.988l3.104-3.098.011.11c0 1.654-1.346 3-3 3l-.115-.012zm8.048-8.032l-3.274 3.268c.212.554.341 1.149.341 1.776 0 2.757-2.243 5-5 5-.631 0-1.229-.13-1.785-.344l-2.377 2.372c1.276.588 2.671.972 4.177.972 7.733 0 11.985-8.449 11.985-8.449s-1.415-2.478-4.067-4.595zm1.431-3.536l-18.619 18.58-1.382-1.422 3.455-3.447c-3.022-2.45-4.818-5.58-4.818-5.58s4.446-7.551 12.015-7.551c1.825 0 3.456.426 4.886 1.075l3.081-3.075 1.382 1.42zm-13.751 10.922l1.519-1.515c-.077-.264-.132-.538-.132-.827 0-1.654 1.346-3 3-3 .291 0 .567.055.833.134l1.518-1.515c-.704-.382-1.496-.619-2.351-.619-2.757 0-5 2.243-5 5 0 .852.235 1.641.613 2.342z"/></svg></span>';
+	const ghostIcon = '<span class="label hidden" title="This heading is non-visible (directly or by parents\' styles)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.885 14.988l3.104-3.098.011.11c0 1.654-1.346 3-3 3l-.115-.012zm8.048-8.032l-3.274 3.268c.212.554.341 1.149.341 1.776 0 2.757-2.243 5-5 5-.631 0-1.229-.13-1.785-.344l-2.377 2.372c1.276.588 2.671.972 4.177.972 7.733 0 11.985-8.449 11.985-8.449s-1.415-2.478-4.067-4.595zm1.431-3.536l-18.619 18.58-1.382-1.422 3.455-3.447c-3.022-2.45-4.818-5.58-4.818-5.58s4.446-7.551 12.015-7.551c1.825 0 3.456.426 4.886 1.075l3.081-3.075 1.382 1.42zm-13.751 10.922l1.519-1.515c-.077-.264-.132-.538-.132-.827 0-1.654 1.346-3 3-3 .291 0 .567.055.833.134l1.518-1.515c-.704-.382-1.496-.619-2.351-.619-2.757 0-5 2.243-5 5 0 .852.235 1.641.613 2.342z"/></svg></span>';
 	
 	report[1] += '<div class="report_section"><ul>';
 	document.body.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function(elem){
@@ -43,6 +43,38 @@ function getHeadings() {
 	return report;
 }
 
+function goToHeading(number){
+	chrome.tabs.executeScript({
+		code: 'var headingNumber = ' + number
+	}, function() {
+		chrome.tabs.executeScript({	code: '(' + scrollToHeading + ')();' });
+	});
+}
+function scrollToHeading(){
+	const element = document.body.querySelector('[data-headingext="'+headingNumber+'"]');
+	const y = element.getBoundingClientRect().top + window.pageYOffset - 100;
+	window.scrollTo({top: y, behavior: 'smooth'});
+}
+
+function generateReport(){
+	chrome.tabs.executeScript({
+		code: '(' + getHeadings + ')();'
+	}, (results) => {
+		if(results === undefined){
+			document.querySelector('#error').style.display = 'block';
+		}else{
+			document.querySelector('#error').style.display = 'none';
+			document.querySelector('#report-type').innerHTML = results[0][0];
+			document.querySelector('#report-order').innerHTML = results[0][1];
+			document.querySelectorAll('.report_title').forEach(function(elem){
+				elem.addEventListener('click', () => {
+					goToHeading(elem.dataset.headingext);
+				});
+			});
+		}
+	});
+}
+
 document.querySelector('#tab-type').addEventListener('click', () => {
 	document.querySelector('#report-order').style.display = 'none';
 	document.querySelector('#report-type').style.display = 'block';
@@ -57,32 +89,10 @@ document.querySelector('#tab-order').addEventListener('click', () => {
 	document.querySelector('#tab-type').classList.remove('active');
 });
 
-function goToHeading(number){
-	chrome.tabs.executeScript({
-		code: 'var headingNumber = ' + number
-	}, function() {
-		chrome.tabs.executeScript({	code: '(' + scrollToHeading + ')();' });
-	});
-}
-function scrollToHeading(){
-	const element = document.body.querySelector('[data-headingext="'+headingNumber+'"]');
-	const y = element.getBoundingClientRect().top + window.pageYOffset - 100;
-	window.scrollTo({top: y, behavior: 'smooth'});
-}
-
-document.querySelectorAll('#start, #refresh').forEach(function(elem){
-	elem.addEventListener('click', () => {
-		chrome.tabs.executeScript({
-			code: '(' + getHeadings + ')();'
-		}, (results) => {
-			document.querySelector('#start-page').style.display = 'none';
-			document.querySelector('#report-type').innerHTML = results[0][0];
-			document.querySelector('#report-order').innerHTML = results[0][1];
-			document.querySelectorAll('.report_title').forEach(function(elem){
-				elem.addEventListener('click', () => {
-					goToHeading(elem.dataset.headingext);
-				});
-			});
-		});
-	});
+document.querySelector('#refresh').addEventListener('click', () => {
+	generateReport();
 });
+
+window.onload = function() {
+	generateReport();
+};
